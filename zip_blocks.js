452eae9d5dd2cf7e4539ed8aa30a82a508f1c8b0
Angelo Polo <polo.language@gmail.com>
2014-12-17 18:55:39 -0500

    Working for first tests.

--- zip_blocks.js
+++ zip_blocks.js
@@ -1,27 +1,35 @@
 'use strict';
 if (process.argv.length < 4 || process.argv.length > 5) {
-  console.log('Usage: node zip_blocks.js path_to_files output_zip [approx_block_size_in_MB]');
-  throw new FatalError('Incorrect arguments supplied.');
+  console.log('Incorrect arguments supplied.');
+  console.log('Usage: node zip_blocks.js path_to_files output_dir [approx_block_size_in_MB]');
+  process.exit();
 }
 
 // setup and defaults:
-var blockSize = 20; // magic number defaults
 var fs = require('fs');
 var path = require('path');
 var files = {};
 var blocks = [];
+var blockSizeUnit = 1000000; // million/MB
+var compressionRatio = 1; // built for files with very low compression ratio
 
 // parse args:
 var pathToFiles = process.argv[2];
-var outputZip = process.argv[3];
-if (process.argv.length > 4) {
-  var inputBlockSize = Number.parseInt(process.argv[4], 10);
-  // TODO: test if we get a 'reasonable' number
-  blockSize = inputBlockSize;
+var outputDir = process.argv[3];
+if (!fs.existsSync(outputDir) || !fs.statSync(outputDir).isDirectory()) {
+  console.warn('"' + outputDir + '" is not a directory, using "' + __dirname + '" instead.');
+  outputDir = __dirname;
 }
-var outputDir = path.dirname(outputZip);
+var blockSize = 20; // default block size in millions of bytes
+if (process.argv.length > 4) {
+  var inputBlockSize = parseInt(process.argv[4], 10);
+  if (1 <= inputBlockSize  && inputBlockSize <= 100) {
+    blockSize = inputBlockSize;
+  } else {
+    console.warn('Approximate block size must be between 1 and 100 MB inclusive. Using default of 20 MB instead.');
+  }
+}
 var logFileName = path.join(outputDir, getDateTimeDashesOnly() + '.log');
-
 function writeToLog(text) {
   fs.appendFile(logFileName, text + '\n');
 }
@@ -35,28 +43,44 @@ fs.readdir(pathToFiles, function (err, listing) {
       files[fullPath] = stat.size;
     }
   }
-  testPrint();
-  //getBlocks();
-  //doZip();
+  getBlocks();
+  doZip();
 });
 
-function testPrint() {
+function getBlocks() {
+  var total = 0;
+  var max = blockSize * blockSizeUnit * compressionRatio;
+  var blockNum = 0;
+  blocks[blockNum] = [];
   for (var key in files) {
-    writeToLog(key + ': ' + files[key]);
+    if (files[key] > max) {
+      writeToLog(key + ' is too big for any block - file skipped.');
+      continue;
+    }
+    total += files[key];
+    if (total > max) {
+      blockNum += 1;
+      blocks[blockNum] = [];
+      total = files[key];
+    }
+    blocks[blockNum].push(key);
   }
 }
 
-function getBlocks() {}
-
 function doZip() {
   var AdmZip = require('adm-zip');
-  var zip = new AdmZip();
-    try {
-    //zip.addLocalFile
-  } catch (err) {
-    writeToLog(err);
+  for (var zipNum = 0; zipNum < blocks.length; ++zipNum) {
+    var zip = new AdmZip();
+    for (var i = 0; i < blocks[zipNum].length; ++i) {
+      try {
+        zip.addLocalFile(blocks[zipNum][i]);
+      } catch (err) {
+        writeToLog('Error zipping ' + blocks[i]);
+        continue;
+      }
+    }
+    zip.writeZip(path.join(outputDir, (zipNum + 1).toString() + '.zip'));
   }
-  zip.writeZip(outputZip);
 }
 
 function getDateTimeDashesOnly() {
@@ -79,18 +103,3 @@ function getDateTimeDashesOnly() {
         second = '0' + second;
     return year + '-' + month + '-' + day + '_' + hour + '-' + minute + '-' + second;   
 }
-
-// notes:
-//zip.addLocalFile('file_on_disk', 'path_in_archive');
-//zip.addLocalFolder('path_on_disk', 'path_in_archive');
-
-    /* async version:
-    fs.stat(listing[i], function (err, stat) {
-      if (err) {
-        writeToLog(err);
-        return;
-      }
-      if (stat.isFile()) {
-        files[listing] = stat.size;
-      }
-    });*/
