314e0945f04fede463503eee34bad0c5fc968204
Angelo Polo <polo.language@gmail.com>
2015-01-02 18:36:32 -0500

    Error handling tested.

--- spec/zip-blocks-spec.js
+++ spec/zip-blocks-spec.js
@@ -1,4 +1,5 @@
 describe('zip-blocks constructor', function() {
+  'use strict';
   var ZipBlocks = require('../zip-blocks');
   var zip = new ZipBlocks();
 
@@ -19,25 +20,52 @@ describe('zip-blocks constructor', function() {
 });
 
 describe('zip-blocks zipFilesInDir', function() {
+  'use strict';
   var ZipBlocks = require('../zip-blocks');
-  var zip = new ZipBlocks();
+  var zip;
 
-  /*beforeEach(function () {
+  beforeEach(function () {
     zip = new ZipBlocks();
-  });*/
+  });
 
   it('should have a "zipFilesInDir" method', function () {
     expect(zip.zipFilesInDir).toEqual(jasmine.any(Function));
   });
 
   it('should accept 1-3 arguments inclusive', function () {
-    expect(function () { zip.zipFilesInDir() }).toThrow();
-    expect(function () { zip.zipFilesInDir('1') }).not.toThrow();
-    expect(function () { zip.zipFilesInDir('1', '2') }).not.toThrow();
-    expect(function () { zip.zipFilesInDir('1', '2', '3') }).not.toThrow();
-    expect(function () { zip.zipFilesInDir('1', '2', '3', '4') }).toThrow();
-    expect(function () { zip.zipFilesInDir('1', '2', '3', '4', '5') }).toThrow();
+    expect(function () { zip.zipFilesInDir(); }).toThrow();
+    expect(function () { zip.zipFilesInDir('1'); }).not.toThrow();
+    expect(function () { zip.zipFilesInDir('1', '2'); }).not.toThrow();
+    expect(function () { zip.zipFilesInDir('1', '2', '3'); }).not.toThrow();
+    expect(function () { zip.zipFilesInDir('1', '2', '3', '4'); }).toThrow();
+    expect(function () { zip.zipFilesInDir('1', '2', '3', '4', '5'); }).toThrow();
   });
 
-  // tests for on('error') listener
+  it('should set and call custom callback on error', function () {
+    var obj = {
+      logError: function (msg) {
+        console.log(msg.toString());
+      }
+    };
+    
+    spyOn(obj, 'logError').andCallThrough();
+
+    // causes error by having too few arguments:
+    expect(function () { zip.zipFilesInDir(); }).toThrow();
+    
+    zip.on('error', obj.logError);
+
+    // causes error by having too few arguments:
+    expect(function () { zip.zipFilesInDir(); }).not.toThrow();
+    expect(zip._errorCallback === obj.logError).toBeTruthy();
+    expect(obj.logError).toHaveBeenCalled();
+  });
+
+  /*it('should create a list of files and file sizes', function () {
+    var inputPath = '01_test_input_files';
+    var outputPath = '02_test_output';
+
+    // how to test ...
+  });*/
+
 });
--- zip-blocks.js
+++ zip-blocks.js
@@ -7,8 +7,8 @@ var path = require('path');
 module.exports = ZipBlocks;
 
 function ZipBlocks() {
-  this._files = {};
-  this._blocks = [];
+  //this._files = {};
+  //this._blocks = [];
   this._BLOCK_SIZE_UNIT = 1000000; // = 1 million = 1 MB
   this._COMPRESSION_RATIO = 1; // built for files with very high compression ratio
 
@@ -20,17 +20,20 @@ function ZipBlocks() {
 
   this.on = function (type, callback) {
     if (type === 'error') {
-      errorCallback = callback;
+      this._errorCallback = callback;
     }
   };
 
-};
+}
 
 
 
 ZipBlocks.prototype.zipDirectories = {};
-ZipBlocks.prototype.zipFilesInDir = function (inputDir, outputDir, blockSize) {
-  var usageString = 'Usage: node zip_blocks.js path_to_files output_dir [approx_block_size_in_MB]';
+ZipBlocks.prototype.zipFilesInDir = function (sourceDir, outputDir /*, blockSize*/) {
+  var usageString = 'Usage: node zip_blocks.js path_to_files output_dir [approx_block_size_in_MB]',
+      files = {},
+      blocks = [];
+
   if (arguments.length < 1 || 3 < arguments.length) {
     this._errorCallback(usageString);
   }
@@ -38,34 +41,24 @@ ZipBlocks.prototype.zipFilesInDir = function (inputDir, outputDir, blockSize) {
   if (outputDir === undefined || !fs.existsSync(outputDir)
                               || !fs.statSync(outputDir).isDirectory()) {
     //console.warn('"' + outputDir + '" is not a directory, using input directory instead.');
-    outputDir = inputDir;
+    outputDir = sourceDir;
   }
   
-  
-  // replace with on('error') listener:
-  /*var _logFileName = path.join(outputDir, getDateTimeDashesOnly() + '.log');
-  function writeLineToLog(text) {
-    fs.appendFile(_logFileName, text + '\n');
-  }*/
-};
+  fs.readdir(sourceDir, function (err, listing) {
+    if (err) {
+      this._errorCallback(err);
+      return;
+    }
+    for (var i = 0; i < listing.length; ++i) {
+      var fullPath = path.join(sourceDir, listing[i]);
+      var stat = fs.statSync(fullPath);
+      if (stat.isFile()) {
+        files[fullPath] = stat.size;
+      }
+    }
+    //getBlocks();
+    //doZip();
+  });
 
-function getDateTimeDashesOnly() {
-    var now     = new Date(); 
-    var year    = now.getFullYear();
-    var month   = now.getMonth() + 1; 
-    var day     = now.getDate();
-    var hour    = now.getHours();
-    var minute  = now.getMinutes();
-    var second  = now.getSeconds(); 
-    if (month.toString().length === 1)
-        month = '0' + month;
-    if (day.toString().length === 1)
-        day = '0' + day;
-    if (hour.toString().length === 1)
-        hour = '0' + hour;
-    if (minute.toString().length === 1)
-        minute = '0' + minute;
-    if (second.toString().length === 1)
-        second = '0' + second;
-    return year + '-' + month + '-' + day + '_' + hour + '-' + minute + '-' + second;   
-}
+  //
+};
